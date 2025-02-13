b1f01a5cb6588986e456e47519d867914de7eb65
anselmosz <anselmoma2005@outlook.com>
2025-02-13 09:54:48 -0300

    added function 'VerificarCampos' at script.js and index.html

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
 let userName = document.getElementById('userName');
 let userEmail = document.getElementById('userEmail');
 let userTel = document.getElementById('userTel');
-let = msgObt = document.getElementById('msgObt');
+let msgObt = document.getElementById('msgObt');
 
+
+function VerificarCampos() {
+  if (userName.value = '') {
+    alert('Preencha todos os campos para enviar sua mensagem!')
+  }
+};
