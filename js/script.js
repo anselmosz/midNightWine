051e4fff591bf8097bb38e31370dd3310e92e5fc
anselmosz <anselmoma2005@outlook.com>
2025-02-13 11:36:41 -0300

    update msgObt styles at script.js

--- js/script.js
+++ js/script.js
@@ -2,15 +2,16 @@ let nome = document.getElementById('userName');
 let email = document.getElementById('userEmail');
 let telefone = document.getElementById('userTel');
 let msgObt = document.getElementById('msgObt');
+let mensagem = document.getElementById("userMsg");
 
 function VerificarCampos(event) {
   event.preventDefault();
-  if (nome.value == '' || telefone.value == '' || email.value == '') {
+  if (nome.value == '' || telefone.value == '' || email.value == '' || mensagem.value == '') {
     msgObt.innerText = "Preencha todos os campos!";
     msgObt.style.color = '#ff0000';
     msgObt.style.fontWeight = '600';
     msgObt.style.textAlign = 'center';
-  } else if (nome.value != '' && telefone.value != '' && email.value != '') {
+  } else {
     msgObt.innerText = "Formulário enviado!";
     msgObt.style.color = '#00ff00';
     msgObt.style.fontWeight = '600';
