fbb0e615341e47625af04a5fa4234a24d1eda83d
anselmosz <anselmoma2005@outlook.com>
2025-02-13 11:07:43 -0300

    update VerfificarCampos() at line 11

--- js/script.js
+++ js/script.js
@@ -3,8 +3,12 @@ let email = document.getElementById('userEmail');
 let telefone = document.getElementById('userTel');
 let msgObt = document.getElementById('msgObt');
 
-function VerificarCampos() {
+function VerificarCampos(event) {
+  event.preventDefault();
   if (nome.value == '' || telefone.value == '' || email.value == '') {
-    alert('Preencha todos os campos para enviar sua mensagem!')
+    // alert('Preencha todos os campos para enviar sua mensagem!')
+    paragrafo.innerText = "Preencha todos os campos!";
+  } else if (nome.value != '' && telefone.value != '' && email.value != '') {
+    paragrafo.innerText = "Formulário enviado com sucesso!";
   }
 };
