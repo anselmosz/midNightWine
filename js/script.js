c3b29ad57d4c5054c3bfc0fe4cd536997c489a0c
anselmosz <anselmoma2005@outlook.com>
2025-02-13 11:18:47 -0300

    update logic on script.js

--- js/script.js
+++ js/script.js
@@ -6,9 +6,8 @@ let msgObt = document.getElementById('msgObt');
 function VerificarCampos(event) {
   event.preventDefault();
   if (nome.value == '' || telefone.value == '' || email.value == '') {
-    // alert('Preencha todos os campos para enviar sua mensagem!')
     msgObt.innerText = "Preencha todos os campos!";
   } else if (nome.value != '' && telefone.value != '' && email.value != '') {
-    msgObt.innerText = "Formulário enviado com sucesso!";
+    msgObt.innerText = "Formulário enviado!";
   }
 };
