86a66e3ed815dc9e458c0d745f49e33d9211ea0b
anselmosz <anselmoma2005@outlook.com>
2025-02-13 09:50:34 -0300

    update script.js

--- js/script.js
+++ js/script.js
@@ -2,3 +2,4 @@ let userName = document.getElementById('userName');
 let userEmail = document.getElementById('userEmail');
 let userTel = document.getElementById('userTel');
 let = msgObt = document.getElementById('msgObt');
+
